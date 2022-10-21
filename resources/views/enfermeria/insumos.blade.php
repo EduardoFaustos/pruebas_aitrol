cf01f5b5b444380614bbd6c0f5e52040a058bfbb
Cristhian Aroni <caroni@mdconsgroup.com>
2022-10-21 10:25:13 -0500

    prueba

--- resources/views/enfermeria/insumos.blade.php
+++ resources/views/enfermeria/insumos.blade.php
@@ -265,9 +265,26 @@
                 <!--Array de Productos-->
                 <?php $resumen = array();
                 foreach ($productos as $producto) {
-                  echo '<tr>
-                  <td>hrfhfh</td>
-                  </tr>';
+                  if (isset($producto->movimiento) && !isset($resumen[$producto->movimiento->producto->nombre])) {
+                    $resumen[$producto->movimiento->producto->nombre] = 1;
+                  } else {
+                    if (isset($producto->movimiento->producto)) {
+                      $resumen[$producto->movimiento->producto->nombre] += 1;
+                    }
+                  }
+                  $cont_checks++;
+                  $eliminar = '';
+                  if (Auth::user()->id_tipo_usuario == 1 && Auth::user()->id_tipo_usuario == 7) {
+                    $eliminar = '<input class="eliminar_producto' . $check_eliminado . '" type="checkbox" value="' . $producto->id . '" />';
+                  }
+                  switch ($producto->movimiento->producto->tipo) {
+                    case 0:
+                      echo '<tr>
+                    <td>HONORARIOS MEDICOS</td>
+                    <td>' . isset($producto->movimiento) ? $producto->movimiento->serie : '' . '</td>
+                    </tr>';
+                      break;
+                  }
                 }
                 ?>
 
