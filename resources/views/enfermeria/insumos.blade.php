868815e82ac2500ca171584a2c64054b9ad3e8e4
Cristhian Aroni <caroni@mdconsgroup.com>
2022-10-21 10:07:36 -0500

    prueba

--- resources/views/enfermeria/insumos.blade.php
+++ resources/views/enfermeria/insumos.blade.php
@@ -263,10 +263,9 @@
               </thead>
               <tbody>
                 <!--Array de Productos-->
-                <?php 
-                $resumen = array(); 
-                foreach($productos as $producto)
-
+                @php $resumen = array(); @endphp
+                @foreach($productos as $producto)
+                @php
                 if(isset($producto->movimiento) and !isset($resumen[$producto->movimiento->producto->nombre]) ) {
                 $resumen[$producto->movimiento->producto->nombre] = 1;
                 } else {
@@ -275,7 +274,11 @@
                 }
                 }
                 $cont_checks++;
-                
+
+                @endphp
+                <?php
+
+
                 $eliminar = '';
                 if (Auth::user()->id_tipo_usuario == 1 && Auth::user()->id_tipo_usuario == 7) {
                   $eliminar = '<input class="eliminar_producto' . $check_eliminado . '" type="checkbox" value="' . $producto->id . '" />';
