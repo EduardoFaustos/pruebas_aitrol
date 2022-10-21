46b4a80e4bfbb578873236590189c8357bb944a7
Cristhian Aroni <caroni@mdconsgroup.com>
2022-10-21 10:11:53 -0500

    prueba

--- resources/views/enfermeria/insumos.blade.php
+++ resources/views/enfermeria/insumos.blade.php
@@ -288,10 +288,10 @@
                     echo `<label for="">HONORARIOS MEDICOS</label>
                     <tr>
                       <td>` . isset($producto->movimiento) ? $producto->movimiento->serie : '' . `</td>
-                      <td>' . isset($producto->movimiento) ? $producto->movimiento->producto->nombre : '' . '</td>
-                      <td>' . $producto->created_at . '</td>
-                      <td>' . $producto->cantidad . '</td>
-                      <td>' . $producto->usuario_crea->apellido1 . ' ' . $producto->usuario_crea->nombre1 . '</td>
+                      <td>` . isset($producto->movimiento) ? $producto->movimiento->producto->nombre : '' . `</td>
+                      <td>` . $producto->created_at . `</td>
+                      <td>` . $producto->cantidad . `</td>
+                      <td>` . $producto->usuario_crea->apellido1 . ' ' . $producto->usuario_crea->nombre1 . `</td>
                       <td><a onclick="eliminar_producto(\'' . $producto->id . '\')" class="btn btn-danger col-md-8 col-sm-8 col-xs-8 btn-margin">Eliminar</a>
                       ' . $eliminar . '                        
                         @endif
